bd64a1876fa22d0178c46bfe7e74ec28d4854f10
charlie <charlie@fuseanalytics.com>
2023-03-27 16:28:36 -0400

    Add more unit tests to verify custom vs default and more checks to verify the cert is generated according to the input.

--- plugin/src/functionalTest/groovy/com/fuseanalytics/gradle/x509/GradleSslCertGenPluginFunctionalTest.groovy
+++ plugin/src/functionalTest/groovy/com/fuseanalytics/gradle/x509/GradleSslCertGenPluginFunctionalTest.groovy
@@ -27,7 +27,7 @@ class GradleSslCertGenPluginFunctionalTest extends Specification {
         new File(projectDir, "settings.gradle")
     }
 
-    def "run generateCert"() {
+    def "run default generateCert"() {
         given:
         settingsFile << ""
         buildFile << """
@@ -72,5 +72,57 @@ certificate {
         ks.size() == 1
         X509Certificate cert = ks.getCertificate("gradle_ssl_cert")
         cert.issuerX500Principal.name == "C=US,ST=State,L=Niceville,OU=IT,O=Some Domain LLC,CN=somedomain.com"
+        cert.getPublicKey().getModulus().bitLength() == 2048
+    }
+
+    def "run custom generateCert"() {
+        given:
+        settingsFile << ""
+        buildFile << """
+plugins {
+    id('com.fuseanalytics.gradle.x509.SslCertGen')
+}
+
+certificate {
+    commonName = "somedomain.com"
+    organization = "Some Domain LLC"
+    organizationUnit = "IT"
+    city = "Niceville"
+    region = "State"
+    country = "US"
+    keyPassword = "Hill of Beans!"
+    keyFile = file("\${buildDir}/x509/custom.pkcs12")
+    keySize = 1024
+    daysValid = 128
+}
+"""
+        File certFile = new File( projectDir, "build/x509/custom.pkcs12")
+        when:
+        GradleRunner runner = GradleRunner.create()
+                .forwardOutput()
+                .withPluginClasspath()
+                .withArguments("generateCert")
+                .withProjectDir(projectDir)
+                .withGradleVersion("5.6.4")
+                .withDebug(true)
+        BuildResult result = runner.build()
+        then:
+        result.task(":generateCert")?.outcome == TaskOutcome.SUCCESS
+        certFile.exists()
+        certFile.length() > 0
+
+        when:
+        KeyStore ks = KeyStore.getInstance("pkcs12")
+        certFile.withInputStream {
+            ks.load( it, "Hill of Beans!".getChars() )
+        }
+        then:
+        ks.containsAlias("gradle_ssl_cert")
+        ks.isKeyEntry("gradle_ssl_cert")
+        !ks.isCertificateEntry("gradle_ssl_cert")
+        ks.size() == 1
+        X509Certificate cert = ks.getCertificate("gradle_ssl_cert")
+        cert.issuerX500Principal.name == "C=US,ST=State,L=Niceville,OU=IT,O=Some Domain LLC,CN=somedomain.com"
+        cert.getPublicKey().getModulus().bitLength() == 1024
     }
 }
