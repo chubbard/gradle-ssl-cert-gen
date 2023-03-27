45ad3aef91072d4d674fcfab7966e7947b411158
charlie <charlie@fuseanalytics.com>
2023-03-26 20:42:16 -0400

    Swapped up the test to verify that the task was created when loading the plugin.  However, if there isn't an extension it really shouldn't create the task.

--- plugin/src/test/groovy/com/fuseanalytics/gradle/x509/GradleSslCertGenPluginTest.groovy
+++ plugin/src/test/groovy/com/fuseanalytics/gradle/x509/GradleSslCertGenPluginTest.groovy
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 import spock.lang.Specification
 
 class GradleSslCertGenPluginTest extends Specification {
-    void "plugin does not create task immediately"() {
+    void "plugin creates generateCert"() {
         given:
         Project project = ProjectBuilder.builder().build()
 
@@ -13,26 +13,6 @@ class GradleSslCertGenPluginTest extends Specification {
         project.plugins.apply("com.fuseanalytics.gradle.x509.SslCertGen")
 
         then:
-        project.tasks.findByName("generateCert") == null
-    }
-    /*
-    void "plugin creates task when extension is present"() {
-        given:
-        Project project = ProjectBuilder.builder().build()
-
-        when:
-        project.extensions.add("certificate", new CertificateExtension().with {
-            it.commonName = "somedomain.com"
-            it.organization = "Some Domain LLC"
-            it.organizationUnit = "Development"
-            it.city = "Niceville"
-            it.region = "State"
-            it.country = "US"
-            it.keyPassword = "Hill.Of.Beans"
-            it
-        } )
-
-        then:
         project.tasks.findByName("generateCert") != null
-    }*/
+    }
 }
